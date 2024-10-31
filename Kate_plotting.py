a7436796f4299ab7be1ea452939fefca69def5d3
Thomas Sievert <tsievert@login-2.ultralight.org>
2024-10-31 14:28:21 -0700

    worked on data processing

--- Kate_plotting.py
+++ Kate_plotting.py
@@ -1,157 +1,161 @@
+import argparse
+import copy
+import glob
 import os
 from pathlib import Path
 
-import matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from matplotlib.animation import PillowWriter
 import numpy as np
-import csv
+from cycler import cycler
+
+cmap_petroff10 = ["#3f90da", "#ffa90e", "#bd1f01", "#94a4a2", "#832db6", "#a96b59", "#e76300", "#b9ac70", "#717581", "#92dadd"]
+plt.rcParams.update({"axes.prop_cycle": cycler("color", cmap_petroff10)})
 
 
+def main(filenames):
+    for filename in filenames:
+        print(filename)
+        loaded_data = np.load(filename)
 
-file_name = os.path.join(str(Path().absolute()), 'hardware_output/QW_w_switch_as_BS1_DCbias_5-00V_RF_zeros.npz')
+        timestampArray = loaded_data['timestamps'] / 60
+        histIndexArray1 = loaded_data['histIndex1']
+        histDataArray1 = loaded_data['histData1']
+        histIndexArray2 = loaded_data['histIndex2']
+        histDataArray2 = loaded_data['histData2']
 
-loaded_data = np.load(file_name)
+        qw_steps_1and2 = np.sum(histDataArray2, axis=0)[500:]
+        qw_steps_3and4 = np.sum(histDataArray1, axis=0)[500:]
 
-timestampArray = loaded_data['timestamps']/60
-histIndexArray1 = loaded_data['histIndex1']
-histDataArray1 = loaded_data['histData1']
-histIndexArray2 = loaded_data['histIndex2']
-histDataArray2 = loaded_data['histData2']
+        # fig, (ax1,ax2) = plt.subplots(2)
+        # ax1.plot(range(len(qw_steps_1and2)), qw_steps_1and2)
+        # ax2.plot(range(len(qw_steps_3and4)), qw_steps_3and4)
 
-fig, (ax1,ax2) = plt.subplots(2)
-#ax1.plot(histDataArray2[0,:])
-#ax2.plot(histDataArray1[0,:])
+        # plot_destdir = os.path.join(str(Path().absolute()), 'plots')
+        # if not os.path.exists(plot_destdir):
+        #     os.makedirs(plot_destdir)
+        # plt.savefig(os.path.join(plot_destdir, f"QW_1to4Steps_bias{filename[filename.find('V')-4:filename.find('V')+1]}.png"))
 
-# ax1.plot(histIndexArray2[0,:],np.sum(histDataArray2,axis=0))
-# ax2.plot(histIndexArray1[0,:],np.sum(histDataArray1,axis=0))
-ax1.plot(range(len(np.sum(histDataArray2,axis=0)))[2350:2400], np.sum(histDataArray2,axis=0)[2350:2400])
-ax2.plot(range(len(np.sum(histDataArray1,axis=0)))[310:450], np.sum(histDataArray1,axis=0)[310:450])
-# step1 #
-# timebin1 idx ≈ 1015 until 1032, timebin2 idx ≈ 1088 until 1104
-# step2 #
-# timebin1 idx ≈ 2231 until 2253, timebin2 idx ≈ 2307 until 2322, timebin3 idx ≈ 2378 until 2400
-# step3 #
-# timebin1 idx ≈ 149 until 163, timebin2 ≈ 220 until 238, timebin3 idx ≈ 290 until 310, timebin4 idx ≈ ????
-# step4 #
-# timebin1 idx ≈  until , timebin2 idx ≈ 
+        # Naive attempt to find time-bins #
+        threshold_med = np.median(np.concatenate([qw_steps_1and2, qw_steps_3and4]))
+        threshold_dev = np.median(np.abs(np.concatenate([qw_steps_1and2, qw_steps_3and4]) - threshold_med))
+        threshold = threshold_med + 7*threshold_dev
+        timebins = dict.fromkeys(range(len([qw_steps_1and2, qw_steps_3and4])))
+        pruned_timebins = copy.deepcopy(timebins)
+        for qw_idx, qw in enumerate([qw_steps_1and2, qw_steps_3and4]):
+            timebins[qw_idx] = list()  # 2 b/c 2 qw steps in each histogram
 
-# ax1.set_xlim([10000,50000])
-# ax2.set_xlim([20000,70000])
+            above_threshold_flag = False
+            timebin_start_idx, timebin_end_idx = 0, 0
 
-# ax1.set_ylim([0,22000])
-# ax2.set_ylim([0,800])
+            for i in range(len(qw)-1):
+                if (
+                    qw[i] < threshold and not above_threshold_flag  # Cuts out standard noise
+                ) or (
+                    qw[i] >= threshold and qw[i+1] < threshold and not above_threshold_flag  # Ignore large single-bin noise fluctuations
+                ):
+                    continue
+                elif qw[i] >= threshold and qw[i+1] >= threshold and not above_threshold_flag:  # Start of timebin found!
+                    above_threshold_flag = True
+                    timebin_start_idx = i
+                elif qw[i] >= threshold and above_threshold_flag:  # Ignore bins where we're inside a timebin as we only want the bin edges
+                    continue
+                elif qw[i] < threshold and above_threshold_flag:  # End of timebin found!
+                    above_threshold_flag = False
+                    timebin_end_idx = i
 
-plt.savefig('test.png')
+                    timebins[qw_idx].append([timebin_start_idx, timebin_end_idx])
 
-# print(f"timestamp array: {np.shape(timestampArray)}")
-# print(f"index array 1: {np.shape(histIndexArray1)}")
-# print(f"hist array 1: {np.shape(histDataArray1)}")
-# print(f"index array 2: {np.shape(histIndexArray2)}")
-# print(f"hist array 2: {np.shape(histDataArray2)}")
+            # Pune timebins to remove spurious timebins at the end of data
+            timebins[qw_idx] = np.array(timebins[qw_idx])
+            timebin_start_difs = timebins[qw_idx][1:, 0] - timebins[qw_idx][:-1, 0]
+            new_steps = np.arange(len(timebins[qw_idx]))[np.concatenate([[False], timebin_start_difs > 1000])]
+            if len(new_steps) == 1:  # Should only be one new step b/c only 2 qw step type per histogram
+                pruned_timebins[qw_idx] = {
+                    0: timebins[qw_idx][:new_steps[0]],
+                    1: timebins[qw_idx][new_steps[0]:]
+                }
+            elif len(new_steps) > 1:
+                pruned_timebins[qw_idx] = {
+                    0: timebins[qw_idx][:new_steps[0]],
+                    1: timebins[qw_idx][new_steps[0]:new_steps[1]]
+                }
+            else:
+                pruned_timebins[qw_idx] = {
+                    0: timebins[qw_idx],
+                    1: [[None, None]]
+                }
 
-# print(f"timestamp array [0]: {timestampArray[0]}")
-# print(f"index array 1 [0]: {histIndexArray1[0]}")
-# print(f"hist array 1 [0]: {histDataArray1[0]}")
-# print(f"index array 2 [0]: {histIndexArray2[0]}")
-# print(f"hist array 2 [0]: {histDataArray2[0]}")
+        # Plot data with initial found timebin edges
+        fig, (ax1,ax2) = plt.subplots(2)
+        fig.suptitle('Data histograms with initial timebins before pruning')
+        ax1.plot(range(len(qw_steps_1and2)), qw_steps_1and2)
+        for i, (timebin_start_idx, timebin_end_idx) in enumerate(timebins[0], 1):
+            ax1.vlines(timebin_start_idx, 0, np.max(qw_steps_1and2), linestyle='dashed', color=cmap_petroff10[i], label=f'idx = {timebin_start_idx}')
+            ax1.vlines(timebin_end_idx, 0, np.max(qw_steps_1and2), linestyle='dashed', color=cmap_petroff10[i], label=f'idx = {timebin_end_idx}')
+        ax1.legend(bbox_to_anchor=(1, 1))
+        ax1.set_title('QW steps 1 and 2')
+        ax2.plot(range(len(qw_steps_3and4)), qw_steps_3and4)
+        for i, (timebin_start_idx, timebin_end_idx) in enumerate(timebins[1]):
+            ax2.vlines(timebin_start_idx, 0, np.max(qw_steps_3and4), linestyle='dashed', color=cmap_petroff10[i], label=f'idx = {timebin_start_idx}')
+            ax2.vlines(timebin_end_idx, 0, np.max(qw_steps_3and4), linestyle='dashed', color=cmap_petroff10[i], label=f'idx = {timebin_end_idx}')
+        ax2.legend(bbox_to_anchor=(1, 0.2))
+        ax2.set_title('QW steps 3 and 4')
 
-qw_steps_1and2 = np.sum(histDataArray2, axis=0)
-qw_steps_3and4 = np.sum(histDataArray1, axis=0)
-print(f"QW steps 1&2: {qw_steps_1and2}")
-print(f"QW steps 1&2: {qw_steps_3and4}")
+        plot_destdir = os.path.join(str(Path().absolute()), 'plots')
+        if not os.path.exists(plot_destdir):
+            os.makedirs(plot_destdir)
+        plt.savefig(os.path.join(plot_destdir, f"QW_1to4Steps_bias{filename[filename.find('V')-4:filename.find('V')+1]}.png"), bbox_inches='tight')
+        plt.savefig(os.path.join(plot_destdir, f"QW_1to4Steps_bias{filename[filename.find('V')-4:filename.find('V')+1]}.pdf"), bbox_inches='tight')
+        plt.close()
 
-# print(np.mean(qw_steps_1and2[:50]))
+        # Build prob distributions for QW steps 1-4
+        prob_dists = {}
+        for qw_idx, qw in enumerate([qw_steps_1and2, qw_steps_3and4]):
 
-# fig, (ax1,ax2) = plt.subplots(2)
+            prob_dists[qw_idx] = {}
+            for step_idx in range(2):
+                
+                tbins = {}
+                for tb_idx, (start_idx, end_idx) in enumerate(pruned_timebins[qw_idx][step_idx]):
+                    if start_idx is None:
+                        tbins[tb_idx] = 0
+                    tbins[tb_idx] = np.sum(qw[start_idx:end_idx])
 
-# ax1.stairs(qw_steps_3and4)
-# ax2.hist(qw_steps_1and2, density=True)
+                total_count = np.sum([val for val in tbins.values()])
+                prob_dists[qw_idx][step_idx] = np.array([val / total_count for val in tbins.values()])
 
-# ax1.set_xlim([10000,50000])
-# ax2.set_xlim([20000,70000])
+        # Plot prob distributions
+        fig, (ax1,ax2,ax3,ax4) = plt.subplots(4)
+        plt.title('Probability distributions')
+        ax1.bar(range(len(prob_dists[0][0])), prob_dists[0][0], label='QW Step 1')
+        ax2.bar(range(len(prob_dists[0][1])),prob_dists[0][1], label='QW Step 2')
+        ax3.bar(range(len(prob_dists[1][0])),prob_dists[1][0], label='QW Step 3')
+        ax4.bar(range(len(prob_dists[1][1])),prob_dists[1][1], label='QW Step 4')
 
-# # ax1.set_ylim([0,22000])
-# # ax2.set_ylim([0,800])
-
-# plt.savefig('test2.png')
-
-
-'''
-fig, (ax1,ax2) = plt.subplots(2)
-for frame in range(len(timestampArray)):
-    ax1.clear()
-    ax2.clear()
-
-    ax1.set_xlim([0,50000])
-    ax2.set_xlim([0,50000])
-
-    ax1.set_ylim([0,700])
-    ax2.set_ylim([0,25])
-
-    ax1.plot(histIndexArray2[frame,:], histDataArray2[frame,:])
-    ax2.plot(histIndexArray1[frame,:], histDataArray1[frame,:])
-
-    plt.pause(2)
-
-plt.show()
-'''
-
-'''
-# STEP 1
-step1_dist = np.zeros((len(timestampArray),2))
-for i in range(len(timestampArray)):
-    step1_tb1 = np.sum(histDataArray2[i,33:53])
-    step1_tb2 = np.sum(histDataArray2[i,63:83])
-    step1_tot = step1_tb1+step1_tb2
-    #step1_dist[i,:] = np.array([step1_tb1/step1_tot, step1_tb2/step1_tot])
-    step1_dist[i,:] = np.array([step1_tb1, step1_tb2])
-
-# STEP 2
-step2_dist = np.zeros((len(timestampArray),3))
-for i in range(len(timestampArray)):
-    step2_tb1 = np.sum(histDataArray2[i,389:409])
-    step2_tb2 = np.sum(histDataArray2[i,418:438])
-    step2_tb3 = np.sum(histDataArray2[i,447:467])
-    step2_tot = step2_tb1+step2_tb2+step2_tb3
-    #step2_dist[i,:] = np.array([step2_tb1/step2_tot, step2_tb2/step2_tot, step2_tb3/step2_tot])
-    step2_dist[i,:] = np.array([step2_tb1, step2_tb2, step2_tb3])
+        ax1.legend(bbox_to_anchor=(1, 1))
+        ax1.set_title('QW step 1')
+        ax2.legend(bbox_to_anchor=(1, 0.8))
+        ax2.set_title('QW step 2')
+        ax1.legend(bbox_to_anchor=(1, 0.6))
+        ax3.set_title('QW step 3')
+        ax4.legend(bbox_to_anchor=(1, 0.4))
+        ax4.set_title('QW step 4')
+        
+        plt.savefig(os.path.join(plot_destdir, f"ProbDists_1to4Steps_bias{filename[filename.find('V')-4:filename.find('V')+1]}.png"), bbox_inches='tight')
+        plt.savefig(os.path.join(plot_destdir, f"ProbDists_1to4Steps_bias{filename[filename.find('V')-4:filename.find('V')+1]}.pdf"), bbox_inches='tight')
+        plt.close()
 
 
-# STEP 3
-step3_dist = np.zeros((len(timestampArray),4))
-for i in range(len(timestampArray)):
-    step3_tb1 = np.sum(histDataArray1[i,144:164])
-    step3_tb2 = np.sum(histDataArray1[i,178:198])
-    step3_tb3 = np.sum(histDataArray1[i,208:228])
-    step3_tb4 = np.sum(histDataArray1[i,236:256])
-    step3_tot = step3_tb1+step3_tb2+step3_tb3+step3_tb4
-    #step3_dist[i,:] = np.array([step3_tb1/step3_tot, step3_tb2/step3_tot, step3_tb3/step3_tot, step3_tb4/step3_tot])
-    step3_dist[i,:] = np.array([step3_tb1, step3_tb2, step3_tb3, step3_tb4])
+if __name__ == '__main__':
+    # parser = argparse.ArgumentParser(
+    #     description='Process the quantum random walk data.'
+    # )
+    # parser.add_argument('--dump', dest='output_dir_path', action='store', default=f'{str(Path().absolute())}/../output_sim/',
+    #     help='Name of the output path in which the processed parquets will be stored.'
+    # )
+    # args = parser.parse_args()
 
-fig, (ax1,ax2,ax3) = plt.subplots(3)
-ax1.plot(timestampArray[:],step1_dist[:,0])
-ax1.plot(timestampArray[:],step1_dist[:,1])
+    filenames = glob.glob(os.path.join(str(Path().absolute()), 'hardware_output/*.npz'))
+    filenames.sort()
+    main(filenames)
 
-ax2.plot(timestampArray[:],step2_dist[:,0])
-ax2.plot(timestampArray[:],step2_dist[:,1])
-ax2.plot(timestampArray[:],step2_dist[:,2])
-
-ax3.plot(timestampArray[:],step3_dist[:,0])
-ax3.plot(timestampArray[:],step3_dist[:,1])
-ax3.plot(timestampArray[:],step3_dist[:,2])
-ax3.plot(timestampArray[:],step3_dist[:,3])
-
-ax1.set_xlim([0,20])
-ax2.set_xlim([0,20])
-ax3.set_xlim([0,20])
-
-#ax1.set_ylim([0,1])
-#ax2.set_ylim([0,1])
-#ax3.set_ylim([0,1])
-
-ax3.set(xlabel='Time (min)')
-
-plt.show()
-'''
